e7ec9d20a06e53695618abb2124c47934ca1cb4b
Zoltán <tothz25@gmail.com>
2021-07-14 17:47:10 +0200

    fixed with at large screen size

--- src/SingleCard.js
+++ src/SingleCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const SingleCard = ({ title, price, info, primary, isAnnual }) => {
   return (
     <section
-      className={`flex flex-col shadow-lg border-2 border-grayishblue-vlight my-8 items-center rounded-xl bg-white p-7 ${
+      className={`flex flex-col shadow-lg border-2 border-grayishblue-vlight my-8 items-center rounded-xl bg-white p-7 lg:w-2/5 ${
         primary
           ? `text-grayishblue-dark`
           : `text-grayishblue-vlight bg-gradient-to-br from-primary-light to-primary-dark lg:my-2`
@@ -34,7 +34,7 @@ const SingleCard = ({ title, price, info, primary, isAnnual }) => {
       <button
         className={`cursor-pointer uppercase mt-10 w-full py-3 rounded-lg ${
           !primary
-            ? `text-primary-dark bg-grayishblue-vlight active:bg-primary-dark active:text-grayishblue-vlight border-grayishblue-vlight border`
+            ? `text-primary-dark bg-grayishblue-vlight active:bg-transparent active:text-grayishblue-vlight border-grayishblue-vlight border`
             : `text-grayishblue-vlight bg-gradient-to-r from-primary-light to-primary-dark active:bg-white active:text-primary-dark border-primary-light active:bg-none border`
         }`}
       >
